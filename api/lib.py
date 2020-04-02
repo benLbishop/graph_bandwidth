ebbb298ea9e98dfd3ee2f61a7e158fb7a0b4f70b
benLbishop <ben.bishop374@gmail.com>
2020-04-01 17:05:15 -0700

    fixed bug where I wasn't reversing the list.

--- api/lib.py
+++ api/lib.py
@@ -6,7 +6,8 @@ FS_KEY = 'bytes_fs'
 TS_KEY = 'bytes_ts'
 
 def getWindowsForDevice(device_uuid, end_time, window_time, num_windows):
-        start_idx = getFirstIdxForDevice(device_uuid, end_time)
+        earliest_time = end_time - (window_time * num_windows)
+        start_idx = getFirstIdxForDevice(device_uuid, end_time, earliest_time)
         if (start_idx < 0):
             # didn't find device id. return full empty list
             return makeEmptyWindows(end_time, window_time, num_windows)
@@ -50,12 +51,16 @@ def getWindowsForDevice(device_uuid, end_time, window_time, num_windows):
             empty_windows = makeEmptyWindows(window_start_time, window_time, num_windows - len(completed_windows))
             completed_windows += empty_windows
 
+        completed_windows.reverse()
         return completed_windows
 
-def getFirstIdxForDevice(target_uuid, end_time):
+def getFirstIdxForDevice(target_uuid, end_time, start_time):
     for i in range(len(BANDWIDTHS)):
         elt = BANDWIDTHS[i]
         if elt[DEVICE_KEY] == target_uuid:
+            # if (elt[TIME_KEY] < start_time):
+            #     # the most recent time for the device is before the earliest possible time we could find.
+            #     return -1
             # find the first point where we're earlier than (or at) end_time
             while i < len(BANDWIDTHS) and elt[DEVICE_KEY] == target_uuid:
                 elt = BANDWIDTHS[i]
